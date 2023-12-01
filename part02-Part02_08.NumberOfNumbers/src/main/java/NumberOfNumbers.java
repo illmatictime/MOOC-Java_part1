2e68a650e0f2c90d77369e6f3e24dbe514634a8d
illmatictime <jerrylanderos@gmail.com>
2023-11-30 21:15:30 -0800

    Java Programming I, Part 2, Problem 08

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,21 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int counter = 0;
+        while(true){
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if(number == 0){
+                break;
+            }
+            if(number != 0){
+                counter = counter + 1;
+                continue;
+            }
+            
+        }
+        System.out.println("Number of numbers: " + counter);
     }
 }
