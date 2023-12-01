a73f4726e9fbce0faa04055e8fa831cae53bffb0
illmatictime <jerrylanderos@gmail.com>
2023-11-30 16:24:14 -0800

    Java Programming 1, Sandbox

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -2,6 +2,22 @@
 public class Sandbox {
 
     public static void main(String[] args) {
-        // Write your program here
+        int number = 0;
+
+        while (true) {
+            number = number + 1;
+
+            if (number >= 5) {
+            break;
+            }
+
+            if (number < 5) {
+            continue;
+            }
+
+        System.out.print(number + " ");
+        }
+
+    System.out.print(number + " ");
     }
 }
