899e2b879544bc7a22616008881bdb8d4c672a50
illmatictime <jerrylanderos@gmail.com>
2023-12-02 13:28:43 -0800

    Java Programming I, Part 2, Problem 11

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,24 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int nonZeroNum = 0;
+        int nonZeroSum = 0;
+        
+        while(true){
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input==0){
+                break;
+            }
+            
+            if(input != 0){
+                nonZeroNum = nonZeroNum + 1;
+                nonZeroSum = nonZeroSum + input;                
+            }
+        }
+        System.out.println("Number of numbers: " + nonZeroNum);            
+        System.out.println("Sum of the numbers: "+ nonZeroSum);
     }
 }
