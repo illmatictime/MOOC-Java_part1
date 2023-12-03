69309813bd55e28aa1afc06bfbeede3060230c68
illmatictime <jerrylanderos@gmail.com>
2023-12-02 16:21:17 -0800

    Java Programming I, Part 2, Problem 12

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,22 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int sum = 0;
+        int counter = 0;
+        
+        while(true){
+            System.out.println("Give a number: ");
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0){
+                break;
+            }
+            
+            sum = sum + input;
+            counter = counter + 1;
+        }
+        double average = ((1.0) * sum )/ counter;
+        System.out.println("Average of the numbers: " + average);
     }
 }
