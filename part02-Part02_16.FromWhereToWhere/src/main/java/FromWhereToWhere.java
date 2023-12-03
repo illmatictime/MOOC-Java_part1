88e4de0e2445ffadf5a733374458287d4fb3cc30
illmatictime <jerrylanderos@gmail.com>
2023-12-03 02:27:50 -0800

    Java Programming I, Part 2, Problem 16

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,17 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.print("Where to?" );
+        int endNum = Integer.valueOf(scanner.nextLine());
+        System.out.print("Where from? ");
+        int startNum = Integer.valueOf(scanner.nextLine());
+        
+        if(startNum<=endNum){
+            for(int i = startNum; i<=endNum; i++){
+                System.out.println(i);
+            }
+        }
+            
+
     }
 }
