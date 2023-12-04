d4fd79dae290eec7af9844a07691e4945cc45d88
illmatictime <jerrylanderos@gmail.com>
2023-12-03 16:24:45 -0800

    Java Programming I, Part 2, Problem 19

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,17 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Give a number: ");
+        int input = Integer.valueOf(scanner.nextLine());
+        int sum = 1;
+        if(input == 0){
+            System.out.println("Factorial: 1");
+        }else{
+            for(int i = 1; i<=input; i++){
+                sum = sum * i;
+            }
+            System.out.println("Factorial: " + sum);
+        }  
     }
 }
