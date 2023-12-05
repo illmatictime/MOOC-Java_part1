b2f78a59bd257f0bbf3c6b5e8a15d2d09b2d82a7
illmatictime <jerrylanderos@gmail.com>
2023-12-05 13:04:04 -0800

    Java Programming I, Part 2, Problem 34, 4/4

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -26,11 +26,19 @@ public class AdvancedAstrology {
         for(int i=1; i<=size; i++){
             printSpaces(size-i);
             printStars(i);
-         }        
+        }        
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        for(int i=1; i<=height; i++){
+            printSpaces(height-i);
+            printStars(i+i-1);
+        }
+        printSpaces(height-2);
+        printStars(3);
+        printSpaces(height-2);
+        printStars(3);
     }
 
     public static void main(String[] args) {
