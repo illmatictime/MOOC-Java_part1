cebdb20737a33cf87867e5a685786a9997d78b02
illmatictime <jerrylanderos@gmail.com>
2023-12-05 20:56:51 -0800

    Java Programming I, Part 3, Problem 09

--- part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
+++ part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -20,5 +20,14 @@ public class GreatestInList {
         System.out.println("");
 
         // implement finding the greatest number in the list here
+        int theLargest = list.get(0);
+        
+        for(int i = 0; i < list.size(); i++){
+            //int number = list.get(i);
+            if(list.get(i) > theLargest){
+                theLargest = list.get(i);
+            }
+        }
+        System.out.println("The greatest number: " + theLargest);
     }
 }
