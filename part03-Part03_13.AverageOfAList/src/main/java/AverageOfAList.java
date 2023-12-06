99fceb965524e5fadd604529720c8f1517e74a25
illmatictime <jerrylanderos@gmail.com>
2023-12-05 22:12:14 -0800

    Java Programming I, Part 3, Problem 13

--- part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -11,6 +11,22 @@ public class AverageOfAList {
         // adding them on a list until user gives -1.
         // Then it computes the average of the numbers on the list
         // and prints it.
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+        double sum = 0;
+
+        for(int value: list){
+            sum = sum+value;
+        }
+        System.out.println("Sum: " + sum/list.size());
     }
 }
