ac1faa668ae040eeec51e194c24fe46415c8afed
illmatictime <jerrylanderos@gmail.com>
2023-12-07 11:29:14 -0800

    Java Programming I, Part 3, Problem 25

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,28 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        String[] username = {"alex", "emma"};
+        String[] password = {"sunshine", "haskell"};
+        
+        System.out.print("Enter username: ");
+        String user = scanner.nextLine();
+        
+        System.out.println("");
+        System.out.print("Enter password: ");
+        String pass = scanner.nextLine();
+        
+        for(String index:username){
+            if(index.equals(user)){
+                for(String index1:password){
+                    if(index1.equals(pass)){
+                        System.out.println("You have successfully logged in!");
+                        return;
+                    }
+                }
+            }
+        }
 
+        System.out.println("Incorrect username or password!");
     }
 }
