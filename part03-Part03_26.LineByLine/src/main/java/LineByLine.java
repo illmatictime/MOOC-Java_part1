eddb5cdc0f184e0647a52be3956730f6684d1fe2
illmatictime <jerrylanderos@gmail.com>
2023-12-07 15:06:18 -0800

    Java Programming I, Part 3, Problem 26

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,26 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        while(true){
+            String input = scanner.nextLine();
+            if (input.equals("")){
+                break;
+            }
+            String[] parts = input.split(" ");
+            for(int i = 0; i < parts.length; i++){
+                System.out.println(parts[i]);
+            }
+        }
+        /*        String input = scanner.nextLine();
+        if(input.equals("")){
+        return;
+        }
+        
+        String[] parts = input.split(" ");
+        System.out.println("");
+        for(int i = 0; i < parts.length; i++){
+        System.out.println(parts[i]);
+        }*/
     }
 }
