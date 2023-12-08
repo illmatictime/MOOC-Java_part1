150d759ec674132849a817319497214c228118cf
illmatictime <jerrylanderos@gmail.com>
2023-12-07 19:14:49 -0800

    Java Programming I, Part 3, Problem 29

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,13 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                return;
+            }
+            String[] parts = input.split(" ");
+            System.out.println(parts[parts.length-1]);
+        }
     }
 }
