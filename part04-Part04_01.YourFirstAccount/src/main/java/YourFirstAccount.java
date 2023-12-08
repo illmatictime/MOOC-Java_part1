6319628714443f44c4f0151f832a0e75c49ec20d
illmatictime <jerrylanderos@gmail.com>
2023-12-08 10:14:03 -0800

    Java Programming I, Part 4, Problem 01

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,10 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account firstAccount = new Account("First account", 100.00);
+        
+        firstAccount.deposit(20.0);
+        
+        System.out.println("The balance on my first Account is now: " + firstAccount);
     }
 }
