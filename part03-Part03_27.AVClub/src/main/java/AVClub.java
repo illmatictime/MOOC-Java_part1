45ab48149b74ee75dcc58020263816620d06b0f4
illmatictime <jerrylanderos@gmail.com>
2023-12-07 16:47:39 -0800

    Java Programming I, Part 3, Problem 27

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,19 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while(true){
+            String input = scanner.nextLine();
+            if (input.equals("")){
+                return;
+            }
+            String[] parts = input.split(" ");
+            for(int i = 0; i < parts.length; i++){
+                if(parts[i].contains("av")){
+                    System.out.println(parts[i]);
+                }/*else if(!parts[i].contains("av")){
+                System.out.println("av wasn't found");
+                }*/
+            }
+        }
     }
 }
