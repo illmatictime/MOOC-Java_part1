4540fb4ce933e27ad34d096068a10c67e41be408
illmatictime <jerrylanderos@gmail.com>
2023-12-07 22:00:28 -0800

    Java Programming I, Part 3, Problem 31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,36 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        String word = "";
+        String tempWord = "";
+        int value = 0;
+        int temp = 0;
 
-
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            
+            String[] parts = input.split(",");
+            word = parts[0];
+            value = Integer.valueOf(parts[1]);
+            /*      System.out.println(value+"/"+parts[1]);
+            System.out.println("word " + parts[0] + " value " + parts[1]);
+            System.out.println("outside if:");
+            System.out.println("temp: " + temp+",value: "+value+", tempWord:"+tempWord+", word:"+word);
+            System.out.println("            ==========        ");*/
+            if(value > temp){
+                temp = value;
+                tempWord = word;
+                /*                System.out.println("Name of the oldest: " + parts[0]);
+                System.out.println("temp: " + temp+",value: "+value+", tempWord:"+tempWord+", word:"+word);*/
+            }
+            /*            System.out.println("            ==========        ");
+            System.out.println("temp: " + temp+",value: "+value+", tempWord:"+tempWord+", word:"+word);
+            System.out.println("            ==========        ");*/
+        }
+        System.out.println("Name of the oldest: " + tempWord);
     }
 }
