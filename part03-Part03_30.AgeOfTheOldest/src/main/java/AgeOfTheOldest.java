c68f35cb66e17310f8cbe759038bca0a79160b42
illmatictime <jerrylanderos@gmail.com>
2023-12-07 19:53:14 -0800

    Java Programming I, Part 3, Problem 30

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,23 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        int value = 0;
+        int temp = 0;
+        
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            
+            String[] parts = input.split(",");
+            value = Integer.valueOf(parts[1]);
+            
+            if(value > temp){
+                temp = value;
+            }            
+        }
+        System.out.println("Age of the oldest: " + temp);
     }
 }
