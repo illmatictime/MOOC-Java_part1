7426e38af06efd9af12cd961046dc5b260081d54
illmatictime <jerrylanderos@gmail.com>
2023-12-09 14:23:20 -0800

    Java Programming I, Part 4, Problem 14

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,6 +5,22 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Statistics statistics = new Statistics();
+        
+        System.out.println("Enter numbers: ");
+        while(true){
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input==-1){
+                break;
+            }
+            
+            statistics.addNumber(input);
+        }
+        
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + statistics.evenSum());
+        System.out.println("Sum of odd numbers: " + statistics.oddSum());
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,50 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+    private int evenSum;
+    private int oddSum;
+    
+    public Statistics(){
+        this.count = 0;
+        this.sum = 0;
+        this.evenSum = 0;
+        this.oddSum = 0;
+    }
+    
+    public void addNumber(int number){
+        //this.sum = this.sum + number;
+        this.count = this.count + 1;
+        
+        if(number % 2 == 0){
+            this.sum = this.sum + number;
+            this.evenSum = this.evenSum + number;
+        }else{
+            this.sum = this.sum + number;
+            this.oddSum = this.oddSum + number;
+        }
+    }
+    
+    public int getCount(){
+        return this.count;
+    }
+    
+    public int sum(){
+        return this.sum;
+    }
+    
+    public int evenSum(){
+        return this.evenSum;
+    }
+    
+    public int oddSum(){
+        return this.oddSum;
+    }
+    
+    public double average(){
+        if(!(this.count <= 0)){
+            return ((1.0*this.sum)/this.count);
+        }
+        return 0;
+    }
 }
