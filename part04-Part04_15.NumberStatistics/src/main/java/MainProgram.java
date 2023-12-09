e031246ad3a24f4012d51a0bd8a5ffaecbba5732
illmatictime <jerrylanderos@gmail.com>
2023-12-09 15:05:58 -0800

    Java Programming I, Part 4, Problem 15 Included both solutions

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -6,6 +6,8 @@ public class MainProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
+        Statistics statisticsEven = new Statistics();
+        Statistics statisticsOdd = new Statistics();
         
         System.out.println("Enter numbers: ");
         while(true){
@@ -13,14 +15,22 @@ public class MainProgram {
             
             if(input==-1){
                 break;
+            }else if(input % 2 == 0){
+                statistics.addNumber(input);
+                statisticsEven.addNumber(input);
+            }else{
+                statistics.addNumber(input);
+                statisticsOdd.addNumber(input);
             }
-            
-            statistics.addNumber(input);
         }
         
+
+        
         System.out.println("Sum: " + statistics.sum());
-        System.out.println("Sum of even numbers: " + statistics.evenSum());
-        System.out.println("Sum of odd numbers: " + statistics.oddSum());
+        System.out.println("Sum of even numbers: " + statisticsEven.sum());
+        System.out.println("Sum of odd numbers: " + statisticsOdd.sum());
+//        System.out.println("Sum of even numbers: " + statistics.evenSum());
+//        System.out.println("Sum of odd numbers: " + statistics.oddSum());
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -2,27 +2,28 @@
 public class Statistics {
     private int count;
     private int sum;
-    private int evenSum;
-    private int oddSum;
+//    private int evenSum;
+//    private int oddSum;
     
     public Statistics(){
         this.count = 0;
         this.sum = 0;
-        this.evenSum = 0;
-        this.oddSum = 0;
+//        this.evenSum = 0;
+//        this.oddSum = 0;
     }
     
     public void addNumber(int number){
         //this.sum = this.sum + number;
         this.count = this.count + 1;
+        this.sum = this.sum + number;
         
-        if(number % 2 == 0){
-            this.sum = this.sum + number;
-            this.evenSum = this.evenSum + number;
-        }else{
-            this.sum = this.sum + number;
-            this.oddSum = this.oddSum + number;
-        }
+//        if(number % 2 == 0){
+//            this.sum = this.sum + number;
+//            this.evenSum = this.evenSum + number;
+//        }else{
+//            this.sum = this.sum + number;
+//            this.oddSum = this.oddSum + number;
+//        }
     }
     
     public int getCount(){
@@ -33,13 +34,13 @@ public class Statistics {
         return this.sum;
     }
     
-    public int evenSum(){
-        return this.evenSum;
-    }
-    
-    public int oddSum(){
-        return this.oddSum;
-    }
+//    public int evenSum(){
+//        return this.evenSum;
+//    }
+//    
+//    public int oddSum(){
+//        return this.oddSum;
+//    }
     
     public double average(){
         if(!(this.count <= 0)){
