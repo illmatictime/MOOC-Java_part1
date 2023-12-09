a099f7a66608ee7ab1f50823eb39cc376a332606
illmatictime <jerrylanderos@gmail.com>
2023-12-09 15:24:25 -0800

    Java Programming I, Part 4, Problem 16, 2/6

--- part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -15,6 +15,14 @@ public class PaymentCard {
         this.balance = openingBalance;
     }
     
+    public void eatAffordably(){
+        this.balance -= 2.60;
+    }
+    
+    public void eatHeartily(){
+        this.balance -= 4.60;
+    }
+    
     public String toString(){
         return "The card has a balance of " + this.balance + " euros"; 
     }
