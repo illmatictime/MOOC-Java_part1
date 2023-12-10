50659beae25f504f1b350a1aeb4b6feefd8ce1b4
illmatictime <jerrylanderos@gmail.com>
2023-12-10 00:50:08 -0800

    Java Programming I, Part 4, Problem 25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,17 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Which file should have its contents printer?");
+        String fileName = scanner.nextLine();
+        
+        try(Scanner file = new Scanner(Paths.get(fileName))){ 
+            while (file.hasNextLine()){
+                String row = file.nextLine();
+                System.out.println(row);
+            }
+        } catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
