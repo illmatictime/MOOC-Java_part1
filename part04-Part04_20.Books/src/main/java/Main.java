48d6a62cccc1f476b5c08d048a1da13edcb9562d
illmatictime <jerrylanderos@gmail.com>
2023-12-09 23:00:15 -0800

    Java Programming I, Part 4, Problem 20 Added two different ways to print out solution

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,48 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        ArrayList<Book> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.print("Title: ");
+            String name = scanner.nextLine();
+            
+            if(name.isEmpty()){
+                break;
+            }
+            
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.print("Publication year:");
+            int publicationYear = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(name, pages, publicationYear));
+        }
+        System.out.println("");
+        System.out.print("What information will be printed? ");
+        String info = scanner.nextLine();
+        
+        for(int i = 0; i < books.size(); i++){
+            //Book book = books.get(i);
+            
+            if(info.equals("everything")){
+                System.out.println(books.get(i));
+            }else if(info.equals("name")){
+                System.out.println(books.get(i).getTitle());
+            }
+        }
+            
+        
+        /*        if(info.equals("everything")){
+        for(Book book:books){
+        System.out.println(book);
+        }
+        }else if(info.equals("name")){
+        for(Book book:books){
+        System.out.println(book.getTitle());
+        }
+        }*/
     }
 }
