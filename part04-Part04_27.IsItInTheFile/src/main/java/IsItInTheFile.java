0f6d609a581645dd5919df51d0d1e2537cbba9d8
illmatictime <jerrylanderos@gmail.com>
2023-12-10 03:29:39 -0800

    Java Programming I, Part 4, Problem 27

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,5 +1,7 @@
 
+import com.sun.tools.javac.code.Attribute;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IsItInTheFile {
@@ -12,6 +14,31 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        ArrayList<String> list = new ArrayList<>();
+        
+        try(Scanner fileReader = new Scanner(Paths.get(file))){
+            //System.out.println("read\n" + fileReader + "\nend");
+            while (fileReader.hasNextLine()){
+                list.add(fileReader.nextLine());
+            }
+        } catch(Exception e){
+            System.out.println("Reading the file " + e.getMessage() + " failed.");
+        }
+        
+        
+        
+     
+        /*        if(searchedFor.isEmpty()){
+        break;
+        }*/
+        System.out.println(list);
+        if(list.contains(searchedFor)){
+            System.out.println("Found!");
+        }else if(!list.contains(searchedFor)){
+            System.out.println("Not found.");
+        }
+        
 
     }
 }
