8e9e4103f2d201ca65258c492192f0f6e3865147
illmatictime <jerrylanderos@gmail.com>
2023-12-09 22:08:31 -0800

    Java Programming I, Part 4, Problem 18

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,27 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            System.out.println("");
+            if(firstName.isEmpty()){
+                break;
+            }
+            
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+            System.out.println("");
+            
+            System.out.print("Identification number: ");
+            String idNum = scanner.nextLine();
+            System.out.println("");
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNum));    
+        }
+        for(PersonalInformation value: infoCollection){
+            System.out.println(value.getFirstName() +" " + value.getLastName());
+            //System.out.println(value);
+        }
     }
 }
