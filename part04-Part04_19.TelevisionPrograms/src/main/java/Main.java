c8b7675b525e55757a40fa8663461a798cea3c71
illmatictime <jerrylanderos@gmail.com>
2023-12-09 22:26:55 -0800

    Java Programming I, Part 4, Problem 19

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,26 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            
+            if(name.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        System.out.println("Program's maximum duration?" );
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for(TelevisionProgram program: programs){
+            if(program.getDuration() <= maxDuration){
+                System.out.println(program);
+            }
+        }
     }
 }
