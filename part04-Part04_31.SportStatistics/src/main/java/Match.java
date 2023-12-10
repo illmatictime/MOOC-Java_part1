09c2dce70e966e00dedb8585258adb457c0d65ee
illmatictime <jerrylanderos@gmail.com>
2023-12-10 15:24:54 -0800

    Java Programming I, Part 4, Problem 31

---
+++ part04-Part04_31.SportStatistics/src/main/java/Match.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yoda
+ */
+public class Match {
+    private String firstTeam;
+    private String secondTeam;
+    private int firstTeamScore;
+    private int secondTeamScore;
+    
+    public Match(String firstTeam, String secondTeam, int firstTeamScore, int SecondTeamScore){
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.firstTeamScore = firstTeamScore;
+        this.secondTeamScore = SecondTeamScore;
+    }
+    
+//    public aMatch(){
+//        
+//    }
+//    
+    public String getFirstTeamName(){
+        return this.firstTeam;
+    }
+    
+    public String getSecondTeamName(){
+        return this.secondTeam;
+    }
+    
+    public int getFirstTeamScore(){
+        return this.firstTeamScore;
+    }
+    
+    public int getSecondTeamScore(){
+        return this.secondTeamScore;
+    }
+    
+    public String toString(){
+        return "This Match was " + this.firstTeam + " against " + this.secondTeam 
+                + " with a score of " + this.firstTeamScore + " to " + this.secondTeamScore; 
+    }
+}
--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,7 +7,43 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.println("File: ");
+        String file = scan.nextLine();
+        
+        ArrayList<Match> matches = readMatchesFromFile(file);
+        System.out.println("Team:");
+        String teamName = scan.nextLine();
+        int count = 0;
+        
+        for(Match team: matches){
+            if(team.toString().contains(teamName)){
+                count = count + 1;
+            }  
+        }
+        System.out.println("Games: " + count);
+        
     }
-
+    
+    public static ArrayList<Match> readMatchesFromFile(String file){
+        ArrayList<Match> match = new ArrayList<>();
+        
+        try(Scanner fileReader = new Scanner(Paths.get(file))){
+            while(fileReader.hasNextLine()){
+                String line = fileReader.nextLine();
+                
+                String[] parts = line.split(",");
+                String firstTeam = parts[0];
+                String secondTeam = parts[1];
+                int firstTeamScore = Integer.valueOf(parts[2]);
+                int secondTeamScore = Integer.valueOf(parts[3]);
+                
+                match.add(new Match(firstTeam, secondTeam, firstTeamScore, secondTeamScore));
+            }
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return match;
+    }
+    
 }
