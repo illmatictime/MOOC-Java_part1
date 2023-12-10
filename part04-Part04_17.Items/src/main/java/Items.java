388c3e976ba7eac9ceb579c5010b5bc83037276b
illmatictime <jerrylanderos@gmail.com>
2023-12-09 21:47:36 -0800

    Java Programming I, Part 4, Problem 17

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -6,9 +6,28 @@ public class Items {
 
     public static void main(String[] args) {
         // implement here your program that uses the class Item
-
-        ArrayList<Item> items = new ArrayList<>();
+        
+        //ArrayList<Item> 
+        //variable list is an arraylist of type Item
+        //<Item> refers to the class name
+        //it is common (99.9%) to create a class file witha the same class name
+        ArrayList<Item> list = new ArrayList<>(); //new object
         Scanner scanner = new Scanner(System.in);
-
+        
+        while(true){
+            System.out.print("Name:");
+            String name = scanner.nextLine();
+            
+            if(name.isEmpty()){
+                break;
+            }
+            //items is object
+            list.add(new Item(name));
+        }
+        
+        System.out.println("");
+        for(Item y : list){
+            System.out.println(y);
+        }   
     }
 }
