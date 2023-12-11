34254e9e1bcd9da5a9f5f70ce46847bb3405f7af
illmatictime <jerrylanderos@gmail.com>
2023-12-11 14:17:15 -0800

    Java Programming I, Part 5, Problem 01

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jlanderos
+ */
+public class Timer {
+    private ClockHand seconds;
+    private ClockHand milliseconds;
+
+    public Timer(){
+        this.seconds = new ClockHand(60);
+        this.milliseconds = new ClockHand(100);
+    }
+    
+    public void advance(){
+        this.milliseconds.advance();
+        
+        if(this.milliseconds.value() == 0){
+            this.seconds.advance();
+        }
+    }
+    
+    public String toString(){
+        return this.seconds + ":" + this.milliseconds;
+    }
+}
