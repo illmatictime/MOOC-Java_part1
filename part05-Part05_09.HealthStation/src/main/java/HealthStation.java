eb8f4f8dde5eefb600c53d48660d6d3524dc1bb7
illmatictime <jerrylanderos@gmail.com>
2023-12-12 16:08:03 -0800

    Java Programming I, Part 5, Problem 09, 3/3

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,14 +1,22 @@
 
 public class HealthStation {
-
+    private int weighingsCounter;
+    
+    public HealthStation(){
+        this.weighingsCounter = 0;
+    }
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        
+        this.weighingsCounter += 1;
         return person.getWeight();
     }
 
     public void feed(Person person){
-        person.setWeight(person.getWeight() +1);
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings(){
+        return weighingsCounter;
     }
 }
