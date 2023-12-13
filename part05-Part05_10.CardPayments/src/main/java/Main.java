193362a2e96907c1d2e032b81da83d435a6fcdc2
illmatictime <jerrylanderos@gmail.com>
2023-12-12 16:35:32 -0800

    Java Programming I, Part 5, Problem 10, 3/4

--- part05-Part05_10.CardPayments/src/main/java/Main.java
+++ part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -2,16 +2,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        PaymentCard petesCard = new PaymentCard(10);
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
 
-        System.out.println("money " + petesCard.balance());
-        boolean wasSuccessful = petesCard.takeMoney(8);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change: " + change);
 
-        wasSuccessful = petesCard.takeMoney(4);
-        System.out.println("successfully withdrew: " + wasSuccessful);
-        System.out.println("money " + petesCard.balance());
+        PaymentCard annesCard = new PaymentCard(7);
+
+        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+
+        System.out.println(unicafeExactum);
     }
 }
 
