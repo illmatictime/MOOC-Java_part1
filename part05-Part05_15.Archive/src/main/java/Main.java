ee150ec549b85218d54959f21ea51a123f4ecdd6
illmatictime <jerrylanderos@gmail.com>
2023-12-14 02:00:07 -0800

    Java Programming I, Part 5, Problem 15, 1/2

--- part05-Part05_15.Archive/src/main/java/Main.java
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,7 +5,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Archive> archives = new ArrayList<>();
+
+        Archive archive;
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String identifier = scanner.nextLine();
+            
+            if (identifier.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Name? (empty will stop)");
+            String name = scanner.nextLine();
+            
+            if(name.isEmpty()){
+                break;
+            }
+
+            archive = new Archive(identifier, name);
 
 
+            archives.add(archive);
+        }
+        System.out.println("==Items==");
+        for(Archive y:archives){
+            System.out.println(y.toString());
+        }
     }
 }
