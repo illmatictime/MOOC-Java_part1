5f24739cb3abba2816b2ef631c645e16677c937b
illmatictime <jerrylanderos@gmail.com>
2023-12-17 02:00:41 -0800

    Java Programming I, Part 6, Problem 03

--- part06-Part06_03.MessagingService/src/main/java/Main.java
+++ part06-Part06_03.MessagingService/src/main/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
+        MessagingService ms = new MessagingService();
+        System.out.println(ms.getMessages());
     }
 }
