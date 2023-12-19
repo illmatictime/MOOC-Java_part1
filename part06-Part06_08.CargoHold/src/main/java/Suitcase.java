f91ce999fb3a6896cdeb6827a0a88b5d929f8bd4
illmatictime <jerrylanderos@gmail.com>
2023-12-18 18:58:31 -0800

    Java Programming I, Part 6, Problem 08, 3/7

---
+++ part06-Part06_08.CargoHold/src/main/java/Suitcase.java
@@ -0,0 +1,58 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yoda
+ */
+
+import java.util.ArrayList;
+
+public class Suitcase {
+    
+    private ArrayList<Item> items;
+    private int maxWeight;
+    
+    public Suitcase(int maxWeight){
+        this.maxWeight = maxWeight;
+        this.items = new ArrayList<>();
+    }
+    
+    public void addItem(Item item){
+//        System.out.println("item.getWeight(): " + item.getWeight());
+        int totalWeight = item.getWeight();
+//        System.out.println("totalWeight: " + totalWeight);
+//        System.out.println("");
+        for(Item value:this.items){
+            totalWeight += value.getWeight();
+//            System.out.println("value.getWeight(): " + value.getWeight());
+//            System.out.println("totalWeight: " + totalWeight);
+//            System.out.println("");
+        }
+        if(this.maxWeight >= totalWeight){
+            this.items.add(item);
+        }
+        
+
+    }
+    
+    public String toString(){
+        //ArrayList<Item> items = new ArrayList<>();
+        int totalWeight = 0;
+        for(int i = 0; i < items.size(); i++){
+            totalWeight += items.get(i).getWeight();
+        }
+        
+        if(items.size() == 0){
+            return "no items (" + totalWeight + " kg)";
+        }else if(items.size() == 1){
+            return this.items.size() + " item (" + totalWeight + " kg)";
+        }
+        return this.items.size() + " items (" + totalWeight + " kg)";
+    }
+  
+}
