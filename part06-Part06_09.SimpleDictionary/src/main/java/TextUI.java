8ab2a28b4956b0c1510d96fd5a19d22fc92e11f1
illmatictime <jerrylanderos@gmail.com>
2023-12-19 16:19:12 -0800

    Java Programming I, Part 6, Problem 09, 4/4 Refactored

--- part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -20,36 +20,78 @@ public class TextUI {
         this.simpleDictionary = simpleDictionary;
     }
     
+//    public void start(){
+//
+//        while(true){
+//            System.out.print("Command: ");
+//            String command = scanner.nextLine();
+//            
+//            if(command.equals("end")){
+//                System.out.println("Bye Bye");
+//                break;
+//            }else if(command.equals("search")){
+//                System.out.print("To be translated: ");
+//                String search = scanner.nextLine();
+//                System.out.println("word to be translated: " + search);
+//                if(this.simpleDictionary.translate(search) == null){
+//                    System.out.println("Word " + search + " was not found");
+//                    continue;
+//                }
+//                System.out.println("Translation: " + this.simpleDictionary.translate(search));
+//                continue;
+//            }else if(command.equals("add")){
+//                System.out.print("Word: ");
+//                String word = scanner.nextLine();
+//                System.out.print("Translation: ");
+//                String translation = scanner.nextLine();
+//                this.simpleDictionary.add(word, translation);
+//                continue;
+//            }else{
+//                System.out.println("Unknown command");
+//                continue;
+//            }
+//        }
+//    }
+    
     public void start(){
-
         while(true){
-            System.out.print("Command: ");
+            System.out.println("Command: ");
             String command = scanner.nextLine();
             
             if(command.equals("end")){
-                System.out.println("Bye Bye");
+                System.out.println("Bye bye!");
                 break;
-            }else if(command.equals("search")){
-                System.out.print("To be translated: ");
-                String search = scanner.nextLine();
-                System.out.println("word to be translated: " + search);
-                if(this.simpleDictionary.translate(search) == null){
-                    System.out.println("Word " + search + " was not found");
-                    continue;
-                }
-                System.out.println("Translation: " + this.simpleDictionary.translate(search));
-                continue;
-            }else if(command.equals("add")){
-                System.out.print("Word: ");
-                String word = scanner.nextLine();
-                System.out.print("Translation: ");
-                String translation = scanner.nextLine();
-                this.simpleDictionary.add(word, translation);
-                continue;
-            }else{
-                System.out.println("Unknown command");
-                continue;
             }
+
+            commandProcessor(command);
+        }
+    }
+    
+    public void commandProcessor(String command){
+        if(command.equals("add")){
+            addTranslation();
+        }else if(command.equals("search")){
+            searchTranslation();
+        }else{
+            System.out.println("Unknown command");
+        }
+    }
+    
+    public void addTranslation(){
+        System.out.print("Word: ");
+        String word = this.scanner.nextLine();
+        System.out.print("Translation: ");
+        String translation = this.scanner.nextLine();
+        this.simpleDictionary.add(word, translation);
+    }
+    
+    public void searchTranslation(){
+        System.out.print("To be translated: ");
+        String search = this.scanner.nextLine();
+        if(this.simpleDictionary.translate(search) == null){
+            System.out.println("Word " + search + " was not found");
+        }else{
+            System.out.println("Translation: " + this.simpleDictionary.translate(search));
         }
     }
 }
