7b0b43993473b1402451ed07973c3753e2e9ebc7
illmatictime <jerrylanderos@gmail.com>
2023-12-22 13:43:39 -0800

    Java I, Section 07, Problem 01, 2/2

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,49 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int container0 = 0;
+        int container1 = 0;
 
         while (true) {
-            System.out.print("> ");
-
+            System.out.println(">");
+            System.out.println("First: " + container0 + "/100");
+            System.out.println("Second: " + container1 + "/100");
+            
             String input = scan.nextLine();
+            
+
             if (input.equals("quit")) {
                 break;
             }
+            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
 
+            if(command.equals("add") && amount>0){
+                container0 += amount;
+                if(container0 > 100){
+                    container0 = 100;
+                }
+            }else if(command.equals("move") && amount>0){
+                if(container0>=amount){
+                    container1 += amount;
+                    container0 -= amount;
+                }else if(amount>container0){
+                    container1 += container0;
+                    container0 = 0;    
+                }
+                if(container1 >= 100){
+                    container1 = 100;
+                }
+
+            }else if(command.equals("remove") && amount>0){
+                container1 -= amount;
+                if(container1 < 0){
+                    container1 = 0;
+                }
+            
+            }
         }
     }
 
