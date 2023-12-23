dc9990623329cf850d2213898e87d1f99aed3708
illmatictime <jerrylanderos@gmail.com>
2023-12-23 14:55:38 -0800

    Finished the problem 7.03

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -6,11 +6,6 @@ public class MainProgram {
         // write your test code here
         int[] array = { 8, 3, 7, 9, 1, 2, 4, 2, 6, 7, 9, 23, 1, 2, 6, 8, 1, 23, 8, 2, 8, 1, 799, 168, 68 };
 
-        // System.out.println("Smallest: " + MainProgram.smallest(array));
-        // System.out.println("Index of the smallest number: " +
-        // MainProgram.indexOfSmallest(array));
-        // System.out.println(MainProgram.indexOfSmallestFrom(array, 2));
-        // System.out.println(Arrays.toString(array));
         System.out.println("start:    " + Arrays.toString(array));
         MainProgram.sort(array);
         System.out.println("sorted:   " + Arrays.toString(array));
@@ -62,13 +57,12 @@ public class MainProgram {
     }
 
     public static void swap(int[] array, int index1, int index2) {
-        int arrayTempIndex = array[index1]; // 2
+        int arrayTempIndex = array[index1];
         array[index1] = array[index2];
         array[index2] = arrayTempIndex;
     }
 
     public static void sort(int[] array) {
-        // 8, 3, 7, 9, 1, 2, 4
         for (int i = 0; i < array.length; i++) {
             System.out.println("sorting:  " + Arrays.toString(array));
             swap(array, indexOfSmallestFrom(array, i), i);
