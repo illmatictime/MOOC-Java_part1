41409caac3f1430a547659cb3cc590d3b2f1f1e8
illmatictime <jerrylanderos@gmail.com>
2023-12-22 16:08:34 -0800

    update

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -3,7 +3,26 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] array = {6, 3, 0, -1, 4 };
+        
+        System.out.println(MainProgram.smallest(array));
+    }
+
+    
+    public static int smallest(int[] array){
+        int smallest = array[0];
+        System.out.println(smallest);
+        System.out.println(array.length);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("for loop: " + array[i]);
+            if(array[i] < smallest){
+                smallest = array[i];
+                System.out.println("smallest: " + smallest);
+            }
+        }
+        
+        System.out.println(smallest);
+        return smallest;
     }
 
 }
