b70247cf35a34675088b4ba81d63545727ed622b
illmatictime <jerrylanderos@gmail.com>
2023-12-23 15:51:52 -0800

    Finished 7.04

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,34 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        // int[] array = { 3, 1, 5, 99, 3, 12 };
+        ArrayList<Integer> array = new ArrayList<>();
+        array.add(8);
+        array.add(3);
+        array.add(7);
+        System.out.println(array);
+        Collections.sort(array);
+        System.out.println(array);
     }
 
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+    }
 }
