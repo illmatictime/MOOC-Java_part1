055bb0090a20476a6cdc2e20c85a5461e58d096e
illmatictime <jerrylanderos@gmail.com>
2023-12-24 17:27:45 -0800

    Update to 7.06

--- part07-Part07_06.GradeStatistics/src/main/java/Grades.java
+++ part07-Part07_06.GradeStatistics/src/main/java/Grades.java
@@ -1,18 +1,45 @@
 import java.util.ArrayList;
 
 public class Grades {
+
     private ArrayList<Integer> grades;
+    private ArrayList<Integer> points;
 
     public Grades() {
         this.grades = new ArrayList<>();
+        this.points = new ArrayList<>();
     }
 
-    public void add(int grade) {
-        if (grade >= 0 && grade <= 100) {
-            this.grades.add(grade);
-        } else {
-            return;
+    public void addPoints(int point) {
+        this.points.add(point);
+    }
+
+    public int averageOfPoints() {
+        if (this.points.isEmpty()) {
+            return -1;
         }
+        int pointsTotal = 0;
+
+        for (Integer integer : points) {
+            pointsTotal += integer;
+        }
+        return pointsTotal / this.points.size();
     }
 
+    public int averagePointsOfPass() {
+        int count = 0;
+        int pointsTotal = 0;
+        for (Integer integer : points) {
+            if (integer >= 50) {
+                pointsTotal += integer;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return -1;
+        }
+
+        return pointsTotal / count;
+    }
 }
--- part07-Part07_06.GradeStatistics/src/main/java/Main.java
+++ part07-Part07_06.GradeStatistics/src/main/java/Main.java
@@ -1,42 +1,16 @@
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Grades> grades = new ArrayList<>();
-        // Write your program here -- consider breaking the program into
-        // multiple classes.
-        System.out.println("Enter point totals, -1 stops: ");
-        int count = 0;
-        int average = 0;
-        int total = 0;
-        int passingGrade = 0;
-        while (true) {
-            int points = Integer.valueOf(scanner.nextLine());
 
-            if (points == -1) {
-                break;
-            }
+        Grades grades = new Grades();
 
-            grades.add(points);
-            // if (points >= 0 && points <= 100) {
-            // total += points;
-            // count++;
-            // average = total / count;
-            // }
-            if (points >= 50) {
-                passingGrade++;
-            }
-        }
+        UserInteface ui = new UserInteface(grades, scanner);
 
-        System.out.println("Point average (all): " + 1.0 * average);
-        if (passingGrade >= 1) {
-            System.out.println("point average (passing):" + passingGrade);
-        }
-        grades.toString();
+        ui.start();
 
     }
 }
