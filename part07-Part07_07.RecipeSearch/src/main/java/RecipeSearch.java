6eeae3fb71dd996a75dd37f5ffef74a85f64d3fe
illmatictime <jerrylanderos@gmail.com>
2023-12-26 16:44:39 -0800

    update to 7.07

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -14,16 +14,13 @@ public class RecipeSearch {
         String file = scanner.nextLine();
         
 
-        Recipes ingredients = new Recipes();
+       // Recipes ingredients = new Recipes();
         
         ArrayList<String> list = new ArrayList<>();
 
         try (Scanner fileReader = new Scanner(Paths.get(file))){
             while(fileReader.hasNextLine()){
                 list.add(fileReader.nextLine());
-                if(fileReader.hasNext("")){
-                    ingredients.add
-                }
             }
         } catch (Exception e) {
             System.out.println("File does not exist: " + e.getMessage());
--- part07-Part07_07.RecipeSearch/src/main/java/Recipes.java
+++ part07-Part07_07.RecipeSearch/src/main/java/Recipes.java
@@ -4,13 +4,12 @@ public class Recipes {
     
     private ArrayList<String> recipes;
 
-    public Recipes(){
-        this.recipes = new ArrayList<>();
+    public Recipes(ArrayList<String> recipe){
+        this.recipes = recipe;
     }
 
     public void add(String recipe){
         this.recipes.add(recipe);
     }
 
-    public void set()
 }
