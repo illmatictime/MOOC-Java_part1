ef336b74352ba50f22da2c317299ad19477021df
illmatictime <jerrylanderos@gmail.com>
2023-12-26 16:29:39 -0800

    Working on 7.07

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,6 +9,27 @@ public class RecipeSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file:");
+
+        String file = scanner.nextLine();
+        
+
+        Recipes ingredients = new Recipes();
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        try (Scanner fileReader = new Scanner(Paths.get(file))){
+            while(fileReader.hasNextLine()){
+                list.add(fileReader.nextLine());
+                if(fileReader.hasNext("")){
+                    ingredients.add
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("File does not exist: " + e.getMessage());
+        }
+
+        System.out.println(list);
     }
 
 }
---
+++ part07-Part07_07.RecipeSearch/src/main/java/Recipes.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Recipes {
+    
+    private ArrayList<String> recipes;
+
+    public Recipes(){
+        this.recipes = new ArrayList<>();
+    }
+
+    public void add(String recipe){
+        this.recipes.add(recipe);
+    }
+
+    public void set()
+}
